4ec9b231e519b50f6b13ae0e87a9110d4c43b8f2
selpeapi <selpeapi@gmail.com>
2020-10-03 23:08:58 +0900

    debug 라벨 삭제 관련 주석 문구 추가 +줄바꿈

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //해당 문구 추가시 앱바 상단의 디버그 라벨 삭제
       debugShowCheckedModeBanner: false,
+
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
